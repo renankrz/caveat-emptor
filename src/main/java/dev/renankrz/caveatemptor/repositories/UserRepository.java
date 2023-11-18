c0d0851b4c646d97029ca860b3c8281c27bac234
renankrz <renankrz@gmail.com>
2023-11-18 11:56:21 -0300

    add methods with custom queries to user repository using @Query

--- src/main/java/dev/renankrz/caveatemptor/repositories/UserRepository.java
+++ src/main/java/dev/renankrz/caveatemptor/repositories/UserRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.util.Streamable;
 
 import dev.renankrz.caveatemptor.model.User;
@@ -66,5 +68,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Streamable<User> findByEmailContaining(String text);
 
     Streamable<User> findByLevel(int level);
+
+    // Query
+
+    @Query("select count(u) from User u where u.active = ?1")
+    int findNumberOfUsersByActive(boolean active);
+
+    @Query("select u from User u where u.level = :level and u.active = :active")
+    List<User> findByLevelAndActive(@Param("level") int level, @Param("active") boolean active);
+
+    @Query(value = "SELECT COUNT(*) FROM users WHERE ACTIVE = ?1", nativeQuery = true)
+    int findNumberOfUsersByActiveNative(boolean active);
+
+    @Query("select u.username, LENGTH(u.email) as email_length from #{#entityName} u where u.username like %?1%")
+    List<Object[]> findByAsArrayAndSort(String text, Sort sort);
 
 }
--- src/test/java/dev/renankrz/caveatemptor/QueryResultsTest.java
+++ src/test/java/dev/renankrz/caveatemptor/QueryResultsTest.java
@@ -1,10 +1,14 @@
 package dev.renankrz.caveatemptor;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 
 import dev.renankrz.caveatemptor.model.User;
 
@@ -19,56 +23,57 @@ public class QueryResultsTest extends CaveatEmptorTests {
         }
     }
 
-    // @Test
-    // void testNumberOfUsersByActivity() {
-    // int active = userRepository.findNumberOfUsersByActivity(true);
-    // int inactive = userRepository.findNumberOfUsersByActivity(false);
+    @Test
+    void testNumberOfUsersByActive() {
+        int active = userRepository.findNumberOfUsersByActive(true);
+        int inactive = userRepository.findNumberOfUsersByActive(false);
 
-    // assertAll(
-    // () -> assertEquals(8, active),
-    // () -> assertEquals(2, inactive));
-    // }
+        assertAll(
+                () -> assertEquals(8, active),
+                () -> assertEquals(2, inactive));
+    }
 
-    // @Test
-    // void testUsersByLevelAndActivity() {
-    // List<User> userList1 = userRepository.findByLevelAndActive(1, true);
-    // List<User> userList2 = userRepository.findByLevelAndActive(2, true);
-    // List<User> userList3 = userRepository.findByLevelAndActive(2, false);
+    @Test
+    void testUsersByLevelAndActive() {
+        List<User> userList1 = userRepository.findByLevelAndActive(1, true);
+        List<User> userList2 = userRepository.findByLevelAndActive(2, true);
+        List<User> userList3 = userRepository.findByLevelAndActive(2, false);
 
-    // assertAll(
-    // () -> assertEquals(2, userList1.size()),
-    // () -> assertEquals(2, userList2.size()),
-    // () -> assertEquals(1, userList3.size()));
-    // }
+        assertAll(
+                () -> assertEquals(2, userList1.size()),
+                () -> assertEquals(2, userList2.size()),
+                () -> assertEquals(1, userList3.size()));
+    }
 
-    // @Test
-    // void testNumberOfUsersByActivityNative() {
-    // int active = userRepository.findNumberOfUsersByActivityNative(true);
-    // int inactive = userRepository.findNumberOfUsersByActivityNative(false);
+    @Test
+    void testNumberOfUsersByActiveNative() {
+        int active = userRepository.findNumberOfUsersByActiveNative(true);
+        int inactive = userRepository.findNumberOfUsersByActiveNative(false);
 
-    // assertAll(
-    // () -> assertEquals(8, active),
-    // () -> assertEquals(2, inactive));
-    // }
+        assertAll(
+                () -> assertEquals(8, active),
+                () -> assertEquals(2, inactive));
+    }
 
-    // @Test
-    // void testFindByAsArrayAndSort() {
-    // List<Object[]> usersList1 = userRepository.findByAsArrayAndSort("ar",
-    // Sort.by("username"));
-    // List<Object[]> usersList2 = userRepository.findByAsArrayAndSort("ar",
-    // Sort.by("email_length").descending());
-    // List<Object[]> usersList3 = userRepository.findByAsArrayAndSort("ar",
-    // JpaSort.unsafe("LENGTH(u.email)"));
-    // assertAll(
-    // () -> assertEquals(2, usersList1.size()),
-    // () -> assertEquals("darren", usersList1.get(0)[0]),
-    // () -> assertEquals(21, usersList1.get(0)[1]),
-    // () -> assertEquals(2, usersList2.size()),
-    // () -> assertEquals("marion", usersList2.get(0)[0]),
-    // () -> assertEquals(26, usersList2.get(0)[1]),
-    // () -> assertEquals(2, usersList3.size()),
-    // () -> assertEquals("darren", usersList3.get(0)[0]),
-    // () -> assertEquals(21, usersList3.get(0)[1]));
-    // }
+    @Test
+    void testFindByAsArrayAndSort() {
+        List<Object[]> usersList1 = userRepository
+                .findByAsArrayAndSort("ar", Sort.by("username"));
+        List<Object[]> usersList2 = userRepository
+                .findByAsArrayAndSort("ar", Sort.by("email_length").descending());
+        List<Object[]> usersList3 = userRepository
+                .findByAsArrayAndSort("ar", JpaSort.unsafe("LENGTH(u.email)"));
+
+        assertAll(
+                () -> assertEquals(2, usersList1.size()),
+                () -> assertEquals("darren", usersList1.get(0)[0]),
+                () -> assertEquals(21, usersList1.get(0)[1]),
+                () -> assertEquals(2, usersList2.size()),
+                () -> assertEquals("marion", usersList2.get(0)[0]),
+                () -> assertEquals(26, usersList2.get(0)[1]),
+                () -> assertEquals(2, usersList3.size()),
+                () -> assertEquals("darren", usersList3.get(0)[0]),
+                () -> assertEquals(21, usersList3.get(0)[1]));
+    }
 
 }
